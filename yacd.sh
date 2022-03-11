3875ec62a9d681e427fd19bb06ca517473eb303f
TuanYZ <48976654+Alamyazid@users.noreply.github.com>
2022-03-11 12:17:34 +0800

    Update yacd.sh

--- yacd.sh
+++ yacd.sh
@@ -2,7 +2,6 @@ pkg update && pkg upgrade -y
 mkdir -p $HOME/.config/clash
 pkg install clash
 pkg install git
-pkg install micro
 cd .config/clash
 git clone https://github.com/Alamyazid/yacd-gh-pages
 cd .config/clash/yacd-gh-pages
