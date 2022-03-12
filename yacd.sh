44c6a930b36886c471a11b37ef4ecea100f298cc
TuanYZ <48976654+Alamyazid@users.noreply.github.com>
2022-03-13 02:20:37 +0800

    Update yacd.sh

--- yacd.sh
+++ yacd.sh
@@ -3,13 +3,18 @@ mkdir -p $HOME/.config/clash
 pkg install clash
 pkg install git
 pkg install micro
+pkg install unzip
 cd .config/clash
 git clone https://github.com/Alamyazid/yacd-gh-pages
 cd yacd-gh-pages
 cp -r -f config.yaml $HOME/.config/clash
 cp -r -f Country.mmdb $HOME/.config/clash
-#cp -r -f vmess.yaml $HOME/.config/clash
-#cp -r -f trojan.yaml $HOME/.config/clash
+cp -r -f vmess.yaml $HOME/.config/clash
+cp -r -f trojan.yaml $HOME/.config/clash
+cd ..
+cd usr/bin
+wget -O menu "https://raw.githubusercontent.com/Alamyazid/yacd-gh-pages/master/menu.sh"
+chmod +x menu
 cd
 rm yacd.sh
 
